f8858cb9665ce8f0544f6bd0641add3084813163
Muhammad Reza <63681345+mreza18@users.noreply.github.com>
2020-04-17 10:13:37 +0800

    Add files via upload

---
+++ belajar_encrypt/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tutorial Cara Membuat Enkripsi URL Parameter Dengan Base64</title>
+</head>
+<body>
+    <h2 align="center">Membuat Enkripsi URL Parameter</h2>
+    <p align="center">Silahkan klik link encrypt atau decrypt di bawah ini;</p>
+    <p align="center">>>> <a href="index.php?id=<?php echo password_hash("halo", PASSWORD_DEFAULT)?>">Enkrip URL</a> <<<</p>
+</body>
+</html>>
